575d7f65701771129c354a38b00fee256cad41b7
RamziAssaf <rassaf@chicagobooth.edu>
2015-10-09 16:20:03 -0500

    In class

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,9 +7,32 @@ class GameController < ApplicationController
     # The move the user chose is in the variable @user_move.
     # ===============================================================
 
-    @computer_move = "Replace this string with the correct value."
 
-    @result = "Replace this string with the correct value."
+    @user_move.downcase
+    if (@user_move=='rock') || (@user_move=='paper') || (@user_move=='scissors')
+        allmoves = ["Rock", "Paper", "Scissors"]
+        @computer_move = allmoves[rand(3)]
+        puts "The computer move is: #{@computer_move}"
+        if @user_move==@computer_move.downcase
+            @result = 'It\'s a tie!'
+        elsif @user_move=='rock' && @computer_move=='Paper'
+            @result = 'You lose!'
+        elsif @user_move=='rock' && @computer_move=='Scissors'
+            @result = 'You win!'
+        elsif @user_move=='paper' && @computer_move=='Rock'
+            @result = 'You win!'
+        elsif @user_move=='paper' && @computer_move=='Scissors'
+            @result = 'You lose!'
+        elsif @user_move=='scissors' && @computer_move=='Rock'
+            @result = 'You lose!'
+        elsif @user_move=='scissors' && @computer_move=='Paper'
+            @result = 'You win!'
+        end
+    else
+        @result = 'That doesn\'t seem to be a valid move.'
+    end
+
+
 
     # ===============================================================
     # Your code goes above.
